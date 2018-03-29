6bd4ea3df7350587480415972fbeffac23ed8c7a
Unknown <emma.n.r56520@gmail.com>
2018-03-29 10:08:01 +0200

    Debut Wordpress
    Fichiers de base, creation wordpress

---
+++ wp-content/themes/toivo/README.md
@@ -0,0 +1,112 @@
+# Toivo
+
+Toivo is perfect fit for business, personal, portfolio or blog sites.
+
+It has beautiful Front Page template where you can show your important content, portfolio, testimonials and Callout links.
+
+Toivo is an accessible, translation-ready theme which supports plenty of features like Site Logo and Schema.org microdata.
+
+## Documentation
+
+Theme has couple of features you should be aware of. You can read documentation in Foxland site.
+
+* [Features documentation](https://foxland.fi/docs/toivo-theme-documentation/)
+* [All documentations](https://foxland.fi/documents/for/toivo/)
+
+## Copyright and License
+
+The following resources are not included with the theme but are external resources linked to within the theme.
+
+* [Lato](https://www.google.com/fonts/specimen/Lato) by Łukasz Dziedzic - Licensed under the [SIL Open Font License, version 1.1](http://scripts.sil.org/OFL).
+* [Raleway](https://www.google.com/fonts/specimen/Raleway) by Multiple Designers - Licensed under the [SIL Open Font License, version 1.1](http://scripts.sil.org/OFL).
+
+The following resources are included within the theme package.
+
+* [Genericons](http://genericons.com/) by Automattic - Licensed under the [GPL, version 2 or later](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html).
+* [Responsive Nav](http://responsive-nav.com/) by Viljami Salminen - Licensed under the [MIT License](http://opensource.org/licenses/MIT).
+
+All other resources and theme elements are licensed under the [GNU GPL](http://www.gnu.org/licenses/old-licenses/gpl-2.0.html), version 2 or later.
+
+&copy; 2015-2016 [Sami Keijonen](https://foxland.fi).
+
+## Changelog
+
+### Version 1.2.0 - December 28, 2016
+
+* Update using WordPress Custom Logo feature. Before logo was from Jetpack.
+* Update media grapper script.
+* Fix skip to content script.
+* Remove rel="designer" from the footer.
+* Add theme support for refresh widgets.
+* Update screen-reader-text class by adding word-wrap: normal.
+
+### Version 1.1.2 - January 29, 2016
+
+* Update multi-level dropdown menu script for better keyboard accessibility.
+
+### Version 1.1.1 - December 23, 2015
+
+* Update multi-level dropdown menu script.
+
+### Version 1.1.0 - September 10, 2015
+
+* Feature: Added setting for disabling multi-level dropdown menu for narrow screens.
+* Fix: Check if Jetpack testimonial title is set. Otherwise there will be undefined notice.
+* Update Media Grabber.
+* Update language files.
+
+### Version 1.0.8 - July 30, 2015
+
+* Tweak: A little smoother navigation toggle.
+* Tweak: Update Breadcrumb Trail to version 1.0.1.
+
+### Version 1.0.7.1 - July 7, 2015
+
+* Fix: Small css bug on social menu.
+* Tweak: Use https to load Google fonts.
+
+### Version 1.0.7 - July 5, 2015
+
+* Fix: You can now use several videos in video post format.
+* Tweak: Much smoother top and social menu experience.
+* Tweak: Add 'toivo-theme' body class. We use this for styling purposes.
+
+### Version 1.0.6 - May 11, 2015
+
+* Add support for Spotify and Soundcloud in Social menu.
+
+### Version 1.0.5 - May 6, 2015
+
+* Remove example.html file from Genericons for security (DOM-based XSS) reasons.
+* Update submenus in Primary menu for enabling toggle support.
+
+### Version 1.0.4 - April 26, 2015
+
+* Change footer text colors for passing #a11y tests.
+* Change default header image color opacity to 70 and add filters around it.
+* Add filter around footer image color opacity.
+* Add better labels for Menu buttons: Top Menu and Social Menu. Primary Menu is just labeled as Menu.
+* Update language files.
+* Remove Twenty Fifteen reference in inc/template-tags.php.
+
+### Version 1.0.3 - April 24, 2015
+
+* Add metabox for replacing Header Callout text and description in singular views.
+
+### Version 1.0.2 - April 19, 2015
+
+* Tweak: Minify styles and scripts for faster loading times.
+* Fix: Update version number in the updater function.
+* Fix: Correct text domain and padding in testimonial archive page.
+* Update .pot file.
+* Change Foxnet to Foxland in the Footer.
+
+### Version 1.0.1 - April 17, 2015
+
+* Add footer section in the Customizer where you can hide or replace footer text.
+* Add Danish translations. Thanks to [Reinar](http://www.reinar-svendsen).
+* CSS tweak: add default top margin to site branding if there is no logo set.
+
+### Version 1.0.0 - April 17, 2015
+
+* Everything's new!
---
+++ wp-content/themes/toivo/js/functions.js
@@ -0,0 +1,141 @@
+
+/**
+ * Enable tab support for dropdown menus.
+ */
+( function() {
+	var container, containerTop, ContainerSocial, menuTop;
+	
+	// Primary menu
+	container = document.getElementById( 'menu-primary' );
+	
+	// Top menu
+	containerTop = document.getElementById( 'menu-top' );
+	
+	// Social menu
+	containerSocial = document.getElementById( 'menu-social' );
+
+	/**
+	 * Make dropdown menus keyboard accessible.
+	 */
+	 
+	if ( container ) {
+		
+		var buttonMain = document.getElementById( 'nav-toggle' );
+		var navMain = responsiveNav(".main-navigation", {                   // Selector
+			transition: 350,                                                // Integer: Speed of the transition, in milliseconds
+			customToggle: "#nav-toggle",                                    // Selector: Specify the ID of a custom toggle
+			enableFocus: true,                                              // Boolean: Do we use use 'focus' class in our nav
+			enableDropdown: navSettings.dropdown,                           // Boolean: Do we use multi level dropdown
+			openDropdown: navSettings.expand,                               // String: Label for opening sub menu
+			closeDropdown: navSettings.collapse,                            // String: Label for closing sub menu
+			resizeMobile: function () {                                     // Set ARIA for menu toggle button
+				buttonMain.setAttribute( 'aria-controls', 'menu-primary' );
+			},
+			resizeDesktop: function () {                                    // Remove ARIA from menu toggle button
+				buttonMain.removeAttribute( 'aria-controls' );
+			},
+		});
+		
+	}
+	
+	if ( containerTop ) {
+
+		var buttonTop = document.getElementById( 'top-nav-toggle' );
+		var navTop = responsiveNav(".top-navigation", { // Selector
+			transition: 350,                                           // Integer: Speed of the transition, in milliseconds
+			customToggle: "#top-nav-toggle",                           // Selector: Specify the ID of a custom toggle
+			enableFocus: true,                                         // Boolean: Do we use use 'focus' class in our nav
+			resizeMobile: function () {                                // Set ARIA for menu toggle button
+				buttonTop.setAttribute( 'aria-controls', 'menu-top' );
+			},
+			resizeDesktop: function () {                               // Remove ARIA from menu toggle button
+				buttonTop.removeAttribute( 'aria-controls' );
+			},
+			open: function () {
+				navSocial.close();
+			},
+		});
+		
+	}
+	
+	if ( containerSocial ) {
+
+		var buttonSocial = document.getElementById( 'social-nav-toggle' );
+		var navSocial = responsiveNav(".social-navigation", { // Selector
+			transition: 350,                                                 // Integer: Speed of the transition, in milliseconds
+			customToggle: "#social-nav-toggle",                              // Selector: Specify the ID of a custom toggle
+			resizeMobile: function () {                                      // Set ARIA for menu toggle button
+				buttonSocial.setAttribute( 'aria-controls', 'menu-social' );
+			},
+			resizeDesktop: function () {                                     // Remove ARIA from menu toggle button
+				buttonSocial.removeAttribute( 'aria-controls' );
+			},
+			open: function () {
+				navTop.close();
+			},
+		});	
+		
+	}
+	
+	// Fix child menus for touch devices.
+	function fixMenuTouchTaps( container ) {
+		var touchStartFn,
+		    parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+
+		if ( 'ontouchstart' in window ) {
+			touchStartFn = function( e ) {
+				var menuItem = this.parentNode;
+
+				if ( ! menuItem.classList.contains( 'focus' ) ) {
+					e.preventDefault();
+					for( var i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if ( menuItem === menuItem.parentNode.children[i] ) {
+							continue;
+						}
+						menuItem.parentNode.children[i].classList.remove( 'focus' );
+					}
+					menuItem.classList.add( 'focus' );
+				} else {
+					menuItem.classList.remove( 'focus' );
+				}
+			};
+
+			for ( var i = 0; i < parentLink.length; ++i ) {
+				parentLink[i].addEventListener( 'touchstart', touchStartFn, false )
+			}
+		}
+	}
+	
+	if ( container ) {
+		fixMenuTouchTaps( container );
+	}
+	
+} )();
+
+/**
+ * Skip link focus fix.
+ */
+(function() {
+	var isIe = /(trident|msie)/i.test( navigator.userAgent );
+
+	if ( isIe && document.getElementById && window.addEventListener ) {
+		window.addEventListener( 'hashchange', function() {
+			var id = location.hash.substring( 1 ),
+				element;
+
+			if ( ! ( /^[A-z0-9_-]+$/.test( id ) ) ) {
+				return;
+			}
+
+			element = document.getElementById( id );
+
+			if ( element ) {
+				if ( ! ( /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) ) {
+					element.tabIndex = -1;
+				}
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
